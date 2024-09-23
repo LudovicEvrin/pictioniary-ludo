f319e1e69d3d9f78c29ced7a638ed8bf727cbd02
Ludovic Evrin <ludovicevrin@gmail.com>
2024-09-23 09:43:58 +0200

    Identification UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pictionairy/ui/screens/identification.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,25 +14,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PICTION.IA.RY',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        scaffoldBackgroundColor: const Color(0xFF48A9A6),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF4357AD),
+            foregroundColor: const Color(0xFFe4dfda),
+            padding: const EdgeInsets.fromLTRB(100, 10, 100, 10),
+          )
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'PICTION.IA.RY'),
+      home: const Identification(),
+    );
+  }
+}
+
+class TitlePage extends StatelessWidget {
+  final String title;
+  const TitlePage({super.key, required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontFamily: 'Arcade',
+        fontSize: 40,
+        color: Color(0xFFe4dfda),
+      ),
+
+
     );
   }
 }
--- lib/ui/screens/identification.dart
+++ lib/ui/screens/identification.dart
@@ -1,2 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:pictionairy/main.dart';
 
+class Identification extends StatelessWidget {
+  const Identification({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const TitlePage(
+          title: 'PICTION.IA.RY',
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF48A9A6),
+      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Entrez votre pseudo',
+                    ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    debugPrint('salu');
+                  },
+                  child: const Text('Enregistrer'),
+              ),
+            ],
+          ),
+        ),
+    );
+  }
+  }
